33299d4b7300650292a2545fc5f2a5653419b96e
Saman Namnik <saman.namnik@gmail.com>
2025-02-13 22:04:15 +0000

    refactored proxy server

--- src/proxy.rs
+++ src/proxy.rs
@@ -8,11 +8,11 @@ use tokio::net::{TcpListener, TcpStream};
 async fn main() -> Result<(), Box<dyn Error>> {
     let listen_addr = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8081".to_string());
+        .unwrap_or_else(|| "127.0.0.1:2748".to_string());
 
     let server_addr = env::args()
         .nth(2)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+        .unwrap_or_else(|| "127.0.0.1:4162".to_string());
 
     println!("Listening on: {listen_addr}");
     println!("Proxying to: {server_addr}");
@@ -22,8 +22,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let mut outbound = TcpStream::connect(server_addr.clone()).await?;
 
         tokio::spawn(async move {
-            //copy_bidirectional is responsible for transferring data in both directions between two streams using internal buffers.
-            //There’s no mechanism in copy_bidirectional to intercept the data being transferred between the two streams. The function doesn’t provide hooks or callbacks to inspect the data during transfer.
+            //transferring data in both directions between two streams using internal buffers.
+            //there’s no mechanism in copy_bidirectional to intercept the data being transferred between the two streams. The function doesn’t provide hooks or callbacks to inspect the data during transfer.
             copy_bidirectional(&mut inbound, &mut outbound)
                 .map(|r| {
                     if let Err(err) = r {
@@ -36,9 +36,3 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
-//How to run
-//1-  cargo run --bin proxy
-//2-  cargo run --bin echo
-//3-  cargo run --bin connect 127.0.0.1:8081
-// type something in the connect terminal.
--- src/samples.rs
+++ src/samples.rs
@@ -7,7 +7,7 @@ use tokio::time::{sleep, Duration};
 use tokio_stream::StreamExt;
 #[tokio::main]
 async fn main() {
-    //-----------------------------------------------------Streams and Adapters
+    //---------------------------------------------------------- Streams and Adapters
     let data = &[1, 2, 3];
     let mut stream = tokio_stream::iter(data);
 
@@ -20,14 +20,13 @@ async fn main() {
         println!("GOT = {:?}", v);
     }
 
-    //--------------------------------------------------------- Task Cancellation
+    //----------------------------------------------------------- Task Cancellation
 
     let (tx, rx) = oneshot::channel();
 
-    //When the spawned task begins, it runs the tokio::select! block
     let task = spawn(async move {
         tokio::select! {
-        last= async_work() =>{ //The => separates the future (or async operation) from the block of code that executes when the future completes
+        last= async_work() =>{
             println!("Work completed!- last: {last}");
 
         }
@@ -38,16 +37,17 @@ async fn main() {
 
     sleep(Duration::from_secs(5)).await;
 
-    //tx.send(()) indicates "Hey, I sent a signal," without transmitting any data. The value doesn't matter if you are only using the channel as a signal.
+    //Hey, I sent a signal, without transmitting any data. The value doesn't matter if you are only using the channel as a signal.
     let _ = tx.send(());
 
-    task.await.unwrap(); //Ensures the spawned task completes. If you don't await the task, it runs in the background.
+    task.await.unwrap(); //ensures the spawned task completes. If you don't await the task, it runs in the background.
 
-    //------------------------------------------------------------- Channels
-    //  If messages are sent faster than they are received, the channel will store them. Once the 32 messages are stored in the channel,
+    //---------------------------------------------------------------- Channels
+
+    //if messages are sent faster than they are received, the channel will store them. Once the 32 messages are stored in the channel,
     // calling send(...).await will go to sleep until a message has been removed by the receiver.
     let (tx, mut rx) = mpsc::channel(32);
-    let tx2 = tx.clone(); //Sending from multiple tasks is done by cloning the Sender
+    let tx2 = tx.clone(); //sending from multiple tasks is done by cloning the Sender
 
     tokio::spawn(async move {
         tx.send("sending from first handle").await.unwrap();
@@ -57,40 +57,41 @@ async fn main() {
         tx2.send("sending from second handle").await.unwrap();
     });
 
-    // /When every Sender has gone out of scope or has otherwise been dropped, it is no longer possible to send more messages into the channel.
-    //At this point, the recv call on the Receiver will return None, which means that all senders are gone and the channel is closed.
+    //when every Sender has gone out of scope or has been dropped, it is no longer possible to send more messages into the channel.at this point,
+    //the recv call on the Receiver will return None, which means that all senders are gone and the channel is closed.
     while let Some(message) = rx.recv().await {
         println!("GOT = {}", message);
     }
 
-    //------------------------------------------- I/O
-    //Read some bytes
-    let mut f = File::open("src/foo.txt").await.unwrap();
+    //------------------------------------------------------------------- I/O
+    //read number of bytes
+    let mut f = File::open("src/read.txt")
+        .await
+        .expect("TODO: make the file first: src/read.txt");
     let mut buffer = [0; 10];
-    //When read() returns Ok(0), this signifies that the stream is closed. Any further calls to read() will complete immediately with Ok(0)
-    let size = f.read(&mut buffer).await.unwrap(); // returning the number of bytes read
+    //when read() returns Ok(0), this signifies that the stream is closed. Any further calls to read() will complete immediately with Ok(0)
+    let size = f.read(&mut buffer).await.unwrap(); //returning the number of bytes read
     println!("The bytes:{size} {:?}", &buffer[..size]);
 
-    //read_to_end
+    //read to end
     let mut buffer = Vec::new();
 
     f.read_to_end(&mut buffer).await.unwrap();
     println!("The bytes {:?}", &buffer[..]);
 
-    //Write
-    let mut f = File::create("src/foo2.txt").await.unwrap();
+    //write
+    let mut f = File::create("src/write.txt").await.unwrap();
 
-    // Writes some prefix of the byte string, but not necessarily all of it.
+    //writes number of bytes.
     let n = f.write(b"bye bytes.").await.unwrap();
     println!("Wrote the first {} bytes of 'bye bytes'.", n);
 
-    //writes the entire buffer into the writer
+    //writes the entire buffer
     f.write_all(b"bye bytes.").await.unwrap();
 
-    //tokio::io::copy asynchronously copies the entire contents of a reader into a writer.
+    //asynchronously copies the entire contents of a reader into a writer.
     let mut reader: &[u8] = b"hello";
-    let mut file = File::create("src/foo.txt").await.unwrap();
-
+    let mut file = File::create("src/readwrite.txt").await.unwrap();
     tokio::io::copy(&mut reader, &mut file).await.unwrap();
 }
 
